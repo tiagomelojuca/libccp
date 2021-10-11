1a3ee7a8e8a2ccf89503918aae01a411283d7e03
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-10-11 03:02:15 -0300

    keep old tests can be dangerous

--- test.bat
+++ test.bat
@@ -1,4 +1,6 @@
 @echo off
+del .\build\test\test_minneg.exe
+del .\build\test\test_punc.exe
 call install
 .\build\test\test_minneg.exe --gtest_color=yes
 .\build\test\test_punc.exe   --gtest_color=yes
