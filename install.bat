9ddfdc8935d135c9310019ee9d7c99bda48d2360
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-10-11 00:42:39 -0300

    enable gdb debugging

--- install.bat
+++ install.bat
@@ -1,4 +1,7 @@
 @echo off
-cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S . -B build
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug ^
+                           -DCMAKE_C_COMPILER=clang ^
+                           -DCMAKE_CXX_COMPILER=clang++ ^
+                           -S . -B build
 cmake --build   build
 cmake --install build
